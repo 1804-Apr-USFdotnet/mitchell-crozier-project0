a608b2744375d4d180964eb18009711c7e80e088
Unknown <mrcrozier@gmail.com>
2018-04-26 19:44:11 -0400

    Added sort methods for viewing all restaurants
    by id and name, both ascending and descending

--- Client/Application.cs
+++ Client/Application.cs
@@ -80,16 +80,69 @@ namespace Client
             }
         }
 
-                private void ViewAllRestaurants()
-                {
-                    Console.WriteLine("\nAll Restaurants:\n");
+        private void ViewAllRestaurants()
+        {
+            Console.WriteLine("\nAll Restaurants:\n");
+            Console.WriteLine("How do you want to sort?\n" +
+                              "1: By restaurantId (Ascending)\n" +
+                              "2: By restaurantId (Descending)\n" +
+                              "3: By RestaurantName (Ascending)\n" +
+                              "4: By RestaurantName (Descending)\n");
+            int input = inOut.ReadInteger();
+            switch (input)
+            {
+                case 1:
+                    SortIdAscending();
+                    break;
+                case 2:
+                    SortIdDescending();
+                    break;
+                case 3:
+                    SortNameAscending();
+                    break;
+                case 4:
+                    SortNameDescending();
+                    break;
+                default:
+                    inOut.Output("\nThat is not an option!\n");
+                    Run();
+                    break;
+            }
 
-                    var results = restaurantService.GetAllRestaurantInfo();
-                    Console.WriteLine("results are : " + results);
-                    var restaurantList = results.Select(x => x.RestaurantName);
+        }
+        private void SortIdAscending()
+        {
+            var results = restaurantService.GetAllRestaurantInfo();
+            Console.WriteLine("results are : " + results);
+            var restaurantList = results.Select(x => x.restaurantId);
 
-                    inOut.Output(restaurantList);
-                }
+            inOut.Output(restaurantList);
+        }
+
+        private void SortIdDescending()
+        {
+            var results = restaurantService.GetAllRestaurantInfo();
+            var restaurantList = results.OrderByDescending(x => x.restaurantId);
+
+            inOut.Output(restaurantList);
+        }
+
+        private void SortNameAscending()
+        {
+            var results = restaurantService.GetAllRestaurantInfo();
+            var restaurantList = results.Select(x => x.RestaurantName);
+
+            inOut.Output(restaurantList);
+        }
+
+        private void SortNameDescending()
+        {
+            var results = restaurantService.GetAllRestaurantInfo();
+            var restaurantList = results.OrderByDescending(x => x.RestaurantName);
+
+            inOut.Output(restaurantList);
+        }
+
         private void TopThreeRatedRestaurants()
         {
             Console.WriteLine("Top three rated \n");
@@ -100,50 +153,79 @@ namespace Client
         private void Search()
         {
             Console.WriteLine("Search for a restaurant \nEnter a full or partial restaurant name:  ");
-            string name = Console.ReadLine();
+            string name = null;
+            try
+            {
+                name = Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input!");
+                loggingService.Log(ex);
+            }
             var results = restaurantService.SearchByName(name);
             inOut.Output(results);
         }
+
         private void AllReviewsForARestaurant()
         {
             Console.WriteLine("All reviews for a restaurant \nEnter the full restaurant name");
-            string name = Console.ReadLine();
+            string name = null;
+            try
+            {
+                name = Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input!");
+                loggingService.Log(ex);
+            }
             var results = restaurantService.AllReviewsForARestauraunt(name);
             var restaurantList = results.Values.ToList();
             int restId = restaurantList[0].restaurantId;
             bool multipleLocations = false;
             foreach (var restaurant in restaurantList)
             {
-                if(restaurant.restaurantId != restId)
+                if (restaurant.restaurantId != restId)
                 {
                     multipleLocations = true;
                 }
             }
-            if(multipleLocations)
-            {                
+            if (multipleLocations)
+            {
                 inOut.Output(restaurantList.Select(rest => rest).Distinct());
                 Console.WriteLine("There is more than one location for the restauraunt you entered!\nInput the id number for the restaurant you wish to select:\n");
-                int input = Convert.ToInt32(Console.ReadLine());
+                int input = -1;
+                try
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Invalid input!");
+                    loggingService.Log(ex);
+                }
                 foreach (var review in results.Keys.ToList())
                 {
-                    if(input == review.restaurantId)
-                    {                             
+                    if (input == review.restaurantId)
+                    {
                         inOut.Output(review);
                     }
                 }
 
-            } else 
+            }
+            else
             {
                 foreach (var review in results.Keys.ToList())
                 {
                     inOut.Output(review);
                 }
             }
-           
-        }              
 
-
-
-            }
         }
-    
+
+
+
+    }
+}
+
